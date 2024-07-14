d4f12510e99e87de3a4a78a86663e419e5d4beed
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2024-07-14 13:15:11 +0800

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -51,6 +51,7 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from numpy import array
+from PIL import Image
 from numpy import argmax
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
@@ -61,5 +62,35 @@ from sklearn.preprocessing import OneHotEncoder
 from easyfsl.samplers import TaskSampler
 from easyfsl.utils import plot_images, sliding_average
 
-##########################################################################
+####################### Load the AIDER Dataset ################################
 
+#### load all images in a directory into memory.
+
+def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
+   return[int(text) if text.isdigit() else text.lower() for text in _nsre.split(s   )]
+
+
+def load_images(path, size = (1024,1024)):
+    data_list = list() # enumerate filenames in directory, assume all are images
+    for filename in sorted(os.listdir(path),key=natural_sort_key):
+      pixels = load_img(path + filename, target_size = size) # Convert to numpy array.
+      pixels = img_to_array(pixels).astype('float32')
+      pixels = cv2.resize(pixels,(128,128)) # Resize to 128 x 128.
+      pixels = pixels/255
+      data_list.append(pixels)
+    return asarray(data_list)
+
+# Five different directory corresponding to 5 different classes.
+
+pathtrain_collapsed = '/content/drive/MyDrive/MAML-keras-master/data/AIDER III/Train/1.collapsed/'
+pathtrain_fire = '/content/drive/MyDrive/MAML-keras-master/data/AIDER III/Train/2.fire/'
+pathtrain_flood = '/content/drive/MyDrive/MAML-keras-master/data/AIDER III/Train/3.flood/'
+pathtrain_traffic = '/content/drive/MyDrive/MAML-keras-master/data/AIDER III/Train/4.traffic/'
+pathtrain_normal = '/content/drive/MyDrive/MAML-keras-master/data/AIDER III/Train/5.normal/'
+
+
+data_train_collapsed = load_images(pathtrain_collapsed)
+data_train_fire = load_images(pathtrain_fire)
+data_train_flood = load_images(pathtrain_flood)
+data_train_traffic  = load_images(pathtrain_traffic)
+data_train_normal = load_images(pathtrain_normal)
