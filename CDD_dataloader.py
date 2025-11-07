24b13d34a6781afd74cbc6401e24a1d1625bf05d
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-11-07 12:00:53 +0800

    Create CDD_dataloader.py

---
+++ CDD_dataloader.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+from torch.utils.data import default_collate
+from torchvision.transforms import v2
+
+#################### Cutmix or MixUp data augmentation ################################
+
+cutmix = v2.CutMix(num_classes=6)
+mixup = v2.MixUp(num_classes=6)
+cutmix_or_mixup = v2.RandomChoice([cutmix, mixup])
+
+def collate_fn(batch):
+    return cutmix_or_mixup(*default_collate(batch))
+
+class CDDCustomDataset(Dataset):
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        image = self.images[idx]
+        image = self.transform(np.array(image))
+        return image, label
+    def __len__(self):
+        return len(self.labels)
+
+###################################################################
+
+train_dataset = CDDCustomDataset(trainarray, trainlabel,transform=data_transform)
+test_dataset = CDDCustomDataset(testarray, testlabel, transform=data_transform_test)
+
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, collate_fn = collate_fn)
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=True)
