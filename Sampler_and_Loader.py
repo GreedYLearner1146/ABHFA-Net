51f8a612c111380490b361b8900baf00d71ce6ef
Garrick Lee <76528869+GreedYLearner1146@users.noreply.github.com>
2025-11-07 12:19:19 +0800

    Update Sampler_and_Loader.py

--- Sampler_and_Loader.py
+++ Sampler_and_Loader.py
@@ -1,4 +1,5 @@
-train_dataset.get_labels = lambda: [instance[1] for instance in train_dataset]
+train_dataset.get_labels = lambda: [instance[1] for instance in train_dataset]  # AIDER/CDD would work since they have the same train_dataset naming 
+                                                                                # as in AIDER_dataloader.py and CDD_dataloader.py
 
 train_sampler = TaskSampler(
     train_dataset, n_way=N_WAY, n_shot=N_SHOT, n_query=N_QUERY, n_tasks=N_TRAINING_EPISODES
